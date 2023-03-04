cb4ecb8930c56ccbe2e0e86d11e259f6d6fa65f0
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-04 15:50:16 -0500

    added seeds, all routes seem to be functioning

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,10 +10,10 @@ router.get('/', (req, res) => {
   Product.findAll(
     {
     include: [
-      {
-      model: Category,
-      model: Tag
-    } 
+      
+     { model: Category },
+     { model: Tag }
+    
     ]
   }
   ).then((allProductData) => {
@@ -35,14 +35,29 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+
+  try {
+    const productData = await Product.create({
+       //do i put id in here?
+      product_name : req.body.product_name,
+      price : req.body.price,
+      stock : req.body.stock,
+       //do i put category_id in here?
+     
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      tagIds: [1, 2, 3, 4]  why tagIds?
     }
+    
   */
   Product.create(req.body)
     .then((product) => {
@@ -108,8 +123,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
